6e4b5538c3428099a1556118014d6054c56ccd90
Tom Himanen <tom.himanen@gmail.com>
2014-04-08 15:47:56 +0300

    FB integration added, plus minor fixes

--- includes/loop.php
+++ includes/loop.php
@@ -3,27 +3,27 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class("post clearfix $class"); ?>>
 
 	<h1 class="post-title"><?php if (is_category() || is_home()) { ?>
-	  <a href="<?php the_permalink()?>#blog"><?php } ?>
+	  <a href="<?php the_permalink()?>"><?php } ?>
 	    <?php the_title(); ?> <span style="font-size: 70%">(<?php the_time('j.n.Y'); ?>)</span>
 	  <?php if (is_category() || is_home()) { ?></a><?php } ?>
 	</h1>
 
-	<p class="post-meta"> 
+	<p class="post-meta">
 		<?php // the_tags(' <span class="post-tag">', ', ', '</span>'); ?>
 		<?php if ( comments_open() ) : ?>
 			<!--<span class="post-comment"><?php comments_popup_link( __( '0 kommenttia', 'themify' ), __( '1 kommentti', 'themify' ), __( '% Comments', 'themify' ) ); ?></span>-->
 		<?php endif; //post comment ?>
 	</p>
-		
-	<?php 
+
+	<?php
 	  the_content();
 	?>
 	<!--<span class="post-author">&ndash; <?php the_author_posts_link() ?></span>-->
 	<!--<br /><time datetime="<?php the_time('o-m-d') ?>" class="post-date" pubdate>[<?php the_time('M j, Y'); ?>]</time>-->
 	<?php if (is_category()) { ?>| [<a href="<?php the_permalink()?>#fb-comments">Kommentoi</a>]<?php } ?>
 	<!--<span class="post-category"><?php the_category(', ') ?></span>-->
-	
+
 	<!--<?php edit_post_link(__('Muokkaa', 'themify'), '[', ']'); ?>-->
-	
+
 </article>
 <!-- /.post -->
--- main.js
+++ main.js
@@ -1,4 +1,27 @@
+
 $(document).ready(function()
 {
-  //$("#helloworld").html("This is Hello World by JQuery");
+  // Slider initialization
+  $('.bxslider').bxSlider(
+  {
+    mode: "fade",
+    pagerCustom: "#navi",
+    controls: false
+  });
+
+  // Scrolling-related functionalities
+  $(document).scroll(function ()
+  {
+    var y = $(this).scrollTop();
+    if (y > 600)
+    {
+      $('#fb-like-side-wrapper').fadeIn();
+    }
+    else
+    {
+      $('#fb-like-side-wrapper').fadeOut();
+    }
+  });
+
+  // Screen-width related functionalities
 });
